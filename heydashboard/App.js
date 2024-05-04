25ffb45d97eb5dd04a63fed9b3cb919d9afc1c16
Marco Lucio <marcoalucio15@gmail.com>
2024-05-04 15:53:04 -0600

    Fixed routing issues.

---
+++ heydashboard/App.js
@@ -0,0 +1,22 @@
+// App.js
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import Encuesta from './screens/Encuesta';
+import Perfil from './screens/Perfil';
+import Dashboard from './screens/Dashboard';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Encuesta" component={Encuesta} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
